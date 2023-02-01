31bac13ab7f31fae75a5d780fc4e26171e7f95d4
doghan <bakhtrian@baktrian>
2023-02-01 23:27:39 +0000

    read pixels to get content of gpu

--- packages/dev/core/src/Helpers/frameBufferHelper.ts
+++ packages/dev/core/src/Helpers/frameBufferHelper.ts
@@ -2,10 +2,11 @@
 import { RenderTargetTexture } from "../Materials/Textures/renderTargetTexture";
 import { Engine } from "../Engines/engine";
 import { Scene } from "../scene";
+import { RawTexture, Texture } from "..";
 
 export class FrameBufferHelper {
   texture: RenderTargetTexture;
-  copyFramebufferToTexture(engine: Engine, scene: Scene) {
+  async copyFramebufferToTexture(engine: Engine, scene: Scene) {
     if(!this.texture) {
       // Create a render target texture
       this.texture = new RenderTargetTexture("framebufferTexture", {
@@ -14,7 +15,11 @@ export class FrameBufferHelper {
       }, scene, false, false, Engine.TEXTURETYPE_UNSIGNED_INT);
     }
     //copy to existing variable
-    this.texture = <RenderTargetTexture>engine._currentFramebuffer;
+    let bufferView = await engine.readPixels(0, 0, engine.getRenderWidth(), engine.getRenderHeight());
+
+    let t = new RawTexture(bufferView, engine.getRenderWidth(), engine.getRenderHeight(), Engine.TEXTUREFORMAT_R, scene, false, false, Texture.TRILINEAR_SAMPLINGMODE)
+    this.texture._buffer = bufferView;
+    // let t2  = new RenderTargetTexture()
     return this.texture;
   }
 }
