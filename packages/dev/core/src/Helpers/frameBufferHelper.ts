bf41904bf1723184ffaad42a4d03c6879a607f0c
doghan <bakhtrian@baktrian>
2023-02-02 06:52:37 +0000

    get framebuffer from gpu

--- packages/dev/core/src/Helpers/frameBufferHelper.ts
+++ packages/dev/core/src/Helpers/frameBufferHelper.ts
@@ -1,11 +1,17 @@
 
 import { RenderTargetTexture } from "../Materials/Textures/renderTargetTexture";
 import { Engine } from "../Engines/engine";
-import { Scene } from "../scene";
-import { RawTexture, Texture } from "..";
+import type { Scene } from "../scene";
 
+/**
+ *
+ */
 export class FrameBufferHelper {
+  /**
+   *
+   */
   texture: RenderTargetTexture;
+
   async copyFramebufferToTexture(engine: Engine, scene: Scene) {
     if(!this.texture) {
       // Create a render target texture
@@ -14,12 +20,9 @@ export class FrameBufferHelper {
         height: engine.getRenderHeight()
       }, scene, false, false, Engine.TEXTURETYPE_UNSIGNED_INT);
     }
-    //copy to existing variable
-    let bufferView = await engine.readPixels(0, 0, engine.getRenderWidth(), engine.getRenderHeight());
-
-    let t = new RawTexture(bufferView, engine.getRenderWidth(), engine.getRenderHeight(), Engine.TEXTUREFORMAT_R, scene, false, false, Texture.TRILINEAR_SAMPLINGMODE)
-    this.texture._buffer = bufferView;
-    // let t2  = new RenderTargetTexture()
+    engine._gl.bindFramebuffer(engine._gl.FRAMEBUFFER, engine._currentFramebuffer);
+    const { FRAMEBUFFER, COLOR_ATTACHMENT0, TEXTURE_2D } = engine._gl;
+    engine._gl.framebufferTexture2D( FRAMEBUFFER, COLOR_ATTACHMENT0, TEXTURE_2D, this.texture, 0);
     return this.texture;
   }
 }
